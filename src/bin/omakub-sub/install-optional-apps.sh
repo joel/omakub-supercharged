e203798f43be9b634c4d84d74e56ee46e38a4142
Joel AZEMAR <joel.azemar@gmail.com>
2025-05-31 11:21:02 +0200

    Enhance icon status function to check for GNOME extensions and improve application choice formatting

--- src/bin/omakub-sub/install-optional-apps.sh
+++ src/bin/omakub-sub/install-optional-apps.sh
@@ -8,6 +8,17 @@ icon_status() {
 
   app_name=$(normalize "$item")
 
+  if [[ "$app_name" == gnome-extension-* ]]; then
+    extension_name="${app_name#gnome-extension-}"
+    if gnome-extensions list | grep -q "$extension_name"; then
+      echo "✔ $item"
+      return
+    else
+      echo "✖ $item"
+      return
+    fi
+  fi
+
   if command -v "$app_name" &> /dev/null; then
     echo "✔ $item"
   else
@@ -16,20 +27,20 @@ icon_status() {
 }
 
 CHOICES=(
-  "$(icon_status 'Code Insiders            Code editor for developers')"
-  "$(icon_status 'Linkquisition            Browser-picker')"
-  "$(icon_status 'Brave Browser            Brave Browser Stable')"
-  "$(icon_status 'Brave Browser Beta       Brave Browser Beta')"
-  "$(icon_status 'Brave Browser Nightly    Brave Browser Nightly')"
-  "$(icon_status 'Firefox                  Firefox Stable')"
-  "$(icon_status 'Firefox Beta             Firefox Beta Channel')"
-  "$(icon_status 'Firefox Developer        Firefox Developer Edition')"
-  "$(icon_status 'Firefox Nightly          Firefox Nightly Channel')"
-  "$(icon_status 'Google Chrome            Google Chrome Stable')"
-  "$(icon_status 'Google Chrome Beta       Google Chrome Beta Channel')"
-  "$(icon_status 'Google Chrome Unstable   Google Chrome Developer (Dev) Channel')"
-  "$(icon_status 'Google Chrome Canary     Google Chrome Canary Channel')"
-  "$(icon_status 'Pano                     Next-gen Clipboard Manager')"
+  "$(icon_status 'Code Insiders                     Code editor for developers')"
+  "$(icon_status 'Linkquisition                   Browser-picker')"
+  "$(icon_status 'Brave Browser                   Brave Browser Stable')"
+  "$(icon_status 'Brave Browser Beta              Brave Browser Beta')"
+  "$(icon_status 'Brave Browser Nightly           Brave Browser Nightly')"
+  "$(icon_status 'Firefox                         Firefox Stable')"
+  "$(icon_status 'Firefox Beta                    Firefox Beta Channel')"
+  "$(icon_status 'Firefox Developer               Firefox Developer Edition')"
+  "$(icon_status 'Firefox Nightly                 Firefox Nightly Channel')"
+  "$(icon_status 'Google Chrome                   Google Chrome Stable')"
+  "$(icon_status 'Google Chrome Beta              Google Chrome Beta Channel')"
+  "$(icon_status 'Google Chrome Unstable          Google Chrome Developer (Dev) Channel')"
+  "$(icon_status 'Google Chrome Canary            Google Chrome Canary Channel')"
+  "$(icon_status 'Gnome Extension pano@elhan.io   Next-gen Clipboard Manager')"
   "<< Back                   "
 )
 
