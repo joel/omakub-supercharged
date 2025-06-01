c0f4a29ff7ef196b0a28f5fc0fa5ecd52fb5779f
Joel AZEMAR <joel.azemar@gmail.com>
2025-06-01 19:30:07 +0200

    Create set-gnome-joel-hotkeys.sh

---
+++ src/install/desktop/set-gnome-joel-hotkeys.sh
@@ -0,0 +1,3 @@
+# Set up custom hotkeys for GNOME workspaces
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Control><Alt>Right']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Control><Alt>Left']"
