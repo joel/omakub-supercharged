beefa5ed8c68864b1d15421b19c086cd8e4a2c7e
Joel Azemar <joel.azemar@gmail.com>
2025-07-28 18:03:41 +0200

    Add Fish shell installation and plugin setup to app-fish.sh

--- src/install/desktop/optional/app-fish.sh
+++ src/install/desktop/optional/app-fish.sh
@@ -9,3 +9,12 @@ sudo apt install -y fish || {
   log_message "ERROR" "Failed to install Fish shell" "$LOG_FILE"
   exit 1
 }
+
+
+log_message "INFO" "Fish installation completed successfully." "$LOG_FILE"
+
+log_message "INFO" "Installing Fish plugins..." "$LOG_FILE"
+
+# Fisher A plugin manager for Fish
+# https://github.com/jorgebucaran/fisher
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
